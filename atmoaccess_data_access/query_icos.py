5cba1f1a569ce9df00fae54c51d7f74042e94015
Pawel Wolff <pawel.wolff@aero.obs-mip.fr>
2025-01-16 15:51:37 +0100

    (fix) datetime64[ns] -> datetime64[ns, UTC]

--- atmoaccess_data_access/query_icos.py
+++ atmoaccess_data_access/query_icos.py
@@ -184,7 +184,7 @@ def query_datasets(variables=[], temporal=[], spatial=[]):
     dtype = dict(zip(stn.columns.tolist(), dtypes))
     # get all datasets and convert dtype
     dataset = __sparql_data()
-    dtypes = ['str', 'str', 'str', 'str', 'int', 'datetime64[ns]', 'datetime64[ns]', 'datetime64[ns]']
+    dtypes = ['str', 'str', 'str', 'str', 'int', 'datetime64[ns, UTC]', 'datetime64[ns, UTC]', 'datetime64[ns, UTC]']
     dtype = dict(zip(dataset.columns.tolist(), dtypes))
     dataset.astype(dtype)
 
